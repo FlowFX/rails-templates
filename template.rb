bdb71e4d9d3f5d4dd94fb0d7dfd1807b75e32a04
Florian Posdziech <hallo@flowfx.de>
2019-05-27 11:00:23 +0200

    Second shot

--- template.rb
+++ template.rb
@@ -1,35 +1,94 @@
+# frozen_string_literal: true
+
 # template.rb
-# https://guides.rubyonrails.org/rails_application_templates.html#route-routing-codex
+# https://guides.rubyonrails.org/rails_application_templates.html
+# https://www.sitepoint.com/rails-application-templates-real-world/
 
+## BUILDING a new Gemfil
+remove_file 'Gemfile'
+run 'touch Gemfile'
+add_source 'https://rubygems.org'
 
-# haml-rails
-# https://github.com/indirect/haml-rails
+gem 'rails', '~> 6.0.0.rc1 '
+# Use sqlite3 as the database for Active Record
+gem 'sqlite3', '~> 1.4'
+# Use Puma as the app server
+gem 'puma', '~> 3.11'
+# Use SCSS for stylesheets
+gem 'sass-rails', '~> 5'
 
-gem 'haml-rails', '~>2.0'
-
-generate('haml:application_layout', 'convert')
-rails_command 'generate haml:application_layout convert'
-run 'rm app/views/layouts/application.html.erb'
-
-# convert all existing views
-rails_command 'haml:erb2haml'
+# Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
+gem 'turbolinks', '~> 5'
+# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
+gem 'jbuilder', '~> 2.5'
+# Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
+gem 'webpacker', '~> 4.0'
+# Reduces boot times through caching; required in config/boot.rb
+gem 'bootsnap', '>= 1.4.2', require: false
+# No more erb foo
+gem 'haml-rails'
 
 # rspec-rails
 gem_group :development, :test do
+  # Call 'byebug' anywhere in the code to stop execution and get a debugger
+  # console.
+  gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'rspec-rails', '~>3.8'
 end
 
-bundle install
-generate('rspec:install')
+gem_group :development do
+  # Access an interactive console on exception pages or by calling 'console'
+  # anywhere in the code.
+  gem 'web-console'
+  gem 'listen'
+  # Spring speeds up development by keeping your application running in the
+  # background.
+  gem 'spring'
+  # cf. https://flowfx.de/blog/speeding-up-rspec-with-bundler-standalone-and-springified-binstubs/
+  gem 'spring-commands-rspec'
+  gem 'spring-watcher-listen'
+end
 
-bundle binstubs rspec-core
-
-generate(:controller, 'Home', 'main')
-
-route "root to: 'home#main'"
+gem_group :test do
+  # Adds support for Capybara system testing and selenium driver
+  gem 'capybara', '>= 2.15'
+  gem 'selenium-webdriver'
+  # Easy installation and use of chromedriver to run system tests with Chrome
+  gem 'chromedriver-helper'
+end
 
 after_bundle do
+  # We're using RSpec
+  remove_dir 'test'
+
+  # We don't need no helper specs.
+  application do
+    <<-RUBY
+    config.generators do |g|
+      g.view_specs true
+      g.helper_specs false
+    end
+    RUBY
+  end
+
+  # Install HAML and convert existing views.
+  generate('haml:application_layout', 'convert')
+  rails_command 'generate haml:application_layout convert'
+  run 'rm app/views/layouts/application.html.erb'
+
+  # #convert all existing views
+  # run 'HAML_RAILS_DELETE_ERB=true rails haml:erb2haml'
+
+  # Install RSpec
+  generate('rspec:install')
+  run 'bundle binstubs rspec-core'
+
+  # Create default controller for the home page
+  generate(:controller, 'Home', 'main')
+  route "root to: 'home#main'"
+
+  # Commit everything to the Git repository
   git :init
   git add: '.'
-  git commit: %Q{ -m 'Initial commit' }
+  git commit: %( -m 'Initial commit' )
 end
